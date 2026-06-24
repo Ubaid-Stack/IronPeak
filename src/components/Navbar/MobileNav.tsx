273ff9a9df5dfd900160d15d12732761f386d341
Ubaid-Stack <ubaidullahfareed2003@gmail.com>
2026-06-25 02:24:11 +0530

    Refactor: Navebar section

--- src/components/Navbar/MobileNav.tsx
+++ src/components/Navbar/MobileNav.tsx
@@ -37,7 +37,7 @@ const MobileNav = ({ isOpen, onClose }: MobileNavProps) => {
       {/* dark background overlay, click to close */}
       <div
         onClick={onClose}
-        className={`fixed inset-0 bg-black/50 z-40 transition-opacity duration-300 md:hidden ${
+        className={`fixed inset-0 bg-black/50 z-40 transition-opacity duration-300 lg:hidden ${
           isOpen
             ? "opacity-100 pointer-events-auto"
             : "opacity-0 pointer-events-none"
@@ -50,12 +50,12 @@ const MobileNav = ({ isOpen, onClose }: MobileNavProps) => {
         role="dialog"
         aria-modal="true"
         aria-label="Mobile navigation"
-        className={`fixed top-0 right-0 h-full w-56 bg-black z-50 p-6 pt-10 transform transition-transform duration-300 md:hidden ${
+        className={`fixed top-0 right-0 h-full w-56 bg-black z-50 p-6 pt-10 transform transition-transform duration-300 lg:hidden ${
           isOpen ? "translate-x-0" : "translate-x-full"
         }`}
       >
         <div className="flex flex-col gap-8">
-          <div className="text-(--color-text-soft) hover:text-white flex justify-end mb-[-15px]">
+          <div className="text-(--color-text-soft) hover:text-white flex justify-end -mb-15px cursor-pointer">
             <X onClick={onClose} />
           </div>
           <NavLinks vertical onLinkClick={onClose} />
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-import { useState } from "react";
-import logo from "../../assets/logo.png";
+import { useState, useEffect } from "react";
+import logo from "../../assets/logo3.png";
 import { PrimaryButton } from "../Buttons";
 import NavLinks from "./NavLinks";
 import MobileNav from "./MobileNav";
@@ -11,7 +11,7 @@ export const AppLogo = () => {
       <img
         src={logo}
         alt="Ironpeak Logo"
-        className="w-10 h-10 object-contain bg-white rounded-full p-1"
+        className="w-10 h-10 object-contain bg-white rounded-xl p-1"
       />
       <h1 className="uppercase text-white text-xl font-bold ml-2">Iron peak</h1>
     </div>
@@ -20,26 +20,40 @@ export const AppLogo = () => {
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+
+    handleScroll();
+    window.addEventListener("scroll", handleScroll);
+
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <header className="fixed top-4 inset-x-0 z-50 px-5">
-      <div className="flex items-center justify-between bg-transparent rounded-full px-4 py-3 max-w-7xl mx-auto">
+    <header className="w-full fixed top-4 inset-x-0 z-50 px-5 lg:px-15">
+      <div
+        className={`border border-white/10 backdrop:backdrop-blur-2xl rounded-xl flex items-center justify-between bg-transparent px-4 py-3 ${scrolled ? "  lg:backdrop-blur-md shadow-lg" : "bg-transparent"}`}
+      >
         <AppLogo />
 
         {/* desktop links — hidden on mobile */}
-        <div className="hidden md:flex">
+        <div className="hidden lg:flex">
           <NavLinks />
         </div>
 
         {/* desktop button — hidden on mobile */}
-        <div className="hidden md:block">
+        <div className="hidden lg:block">
           <PrimaryButton text="Join Now" />
         </div>
 
         {/* hamburger button — only on mobile */}
         <button
           onClick={() => setIsOpen(true)}
-          className="md:hidden text-white text-2xl mr-0.5"
+          className="lg:hidden text-white text-2xl mr-0.5"
           aria-label="Open menu"
           aria-expanded={isOpen}
           aria-controls="mobile-menu"
